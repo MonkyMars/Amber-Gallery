e1c1525ba4b69309ebe4e3b3bead95fba44d01d4
Levi Noppers <levinoppers69@gmail.com>
2024-11-05 22:14:26 +0100

    basic tools and interfaces for user

---
+++ utils/user-service.ts
@@ -0,0 +1,37 @@
+export interface User {
+  id: number;
+  email: string;
+  name: string;
+  token: string;
+}
+
+export interface Credentials {
+  email: string;
+  password: string;
+}
+
+export const SetUser = (user: User, token: string) => {
+  localStorage.setItem('user', JSON.stringify(user));
+  localStorage.setItem('token', token);
+}; 
+
+export const getUser = (): User | null => {
+  const user = localStorage.getItem('user');
+  return user ? JSON.parse(user) : null;
+};
+
+export const GetToken = (): string | null => {
+  const token = localStorage.getItem('token');
+  return token ? token : null;
+};
+
+export const Logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('user');
+  window.location.href = '/user/login';
+};
+
+export const IsLoggedIn = (): boolean => {
+  const token = GetToken();
+  return token ? true : false;
+};
