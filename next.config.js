b11b0d428c5ef49ef3cf3d6938a58261a8e54098
Levi Noppers <levinoppers69@gmail.com>
2024-11-05 22:11:07 +0100

    added image domain and env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    POSTGRES_URL: process.env.POSTGRES_URL,
+  },
+  images: {
+    domains: ['res.cloudinary.com'],
+  },
+}
 
 module.exports = nextConfig
