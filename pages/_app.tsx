8d6bc4eecda4b2e2f2435666cf6632da839c36b8
Levi Noppers <levinoppers69@gmail.com>
2024-11-06 22:15:28 +0100

    added analytics and speed insights from vercel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { ThemeProvider } from '../utils/ThemeContext';
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider>
+      <SpeedInsights />
+      <Analytics />
       <Component {...pageProps} />
     </ThemeProvider>
   )
